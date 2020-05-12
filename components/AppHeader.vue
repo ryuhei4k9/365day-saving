7306a22a15284fdd6298140134a65066cb559644
ryuhei4k9 <ryuhei.4k9@gmail.com>
2020-05-12 22:32:57 +0900

    commit

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="sticky top-0 z-40 bg-teal-500 w-full h-16 overflow-x-hidden border-solid border-b-2 border-gray-600"
+    class="sticky top-0 z-40 bg-teal-500 w-full h-16 p-1 overflow-x-hidden border-solid border-b-2 border-gray-600"
   >
     <span class="font-semibold text-xl tracking-wide text-gray-100 text-xl"
       >365日貯金</span
@@ -12,10 +12,12 @@
 <script>
 import Vue from 'vue'
 import Total from '~/components/Total'
+// import MenuIcon from '~/assets/svg/icon-menu.svg'
 
 export default Vue.extend({
   components: {
     Total
+    // MenuIcon
   }
 })
 </script>
